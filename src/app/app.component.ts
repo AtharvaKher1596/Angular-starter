bf9d7a436a4dda5e5f928f6fa6e67a720d167f95
Atharva Kher <atharvakher@benchmarkit.solutions>
2023-10-13 14:35:50 +0530

    lesson 1: angular directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { WishItem } from './shared/models/wishitem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']  
 })
 export class AppComponent {
-  title = 'Test-App';
+    items : WishItem[] = [
+      new WishItem('To learn angular'),
+      new WishItem('Have a lunch', true),
+      new WishItem('Find grass that cuts itself')
+    ]
+  title = 'wishlist'; 
 }
---
+++ src/app/shared/models/wishitem.ts
@@ -0,0 +1,6 @@
+export class WishItem{
+    constructor(public wishText : string, public isCompleted : boolean = false){
+
+    }
+    
+}
